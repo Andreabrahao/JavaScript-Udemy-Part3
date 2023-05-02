492ef8aaf0a3166f857abd653a198d321a4b9f50
André Abrahão <abrahaoam@gmail.com>
2023-05-02 16:45:08 -0300

    Exercicios

---
+++ Exercicio_1/scripts.js
@@ -0,0 +1,7 @@
+let nome = "Andre";
+let idade = 28;
+let homem = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof homem);
---
+++ Exercicio_3/scripts.js
@@ -0,0 +1,5 @@
+const nome = "Andre";
+
+if(nome == "Andre") {
+    console.log(`Ola ${nome}, tudo bem`);
+}
